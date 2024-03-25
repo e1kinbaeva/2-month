b565aadb8430ed1a88a40d900bfe7144b79d7cf5
e1kinbaeva <ainazikerkinbaeva2106200414@gmail.com>
2024-03-25 19:05:40 +0300

    Добавила  команду GIT

--- lesson_7.py
+++ lesson_7.py
@@ -1,1 +1,25 @@
-print ("leson 7")
+"""
+git --version
+
+git config --global user.name "YOUR USERNAME ON GITHUB"
+
+git config --global user.email "YOUR EMAIL ON GITHUB"
+"""
+
+"""
+git init
+
+git add .
+
+git status
+
+git commit -m "First Commit"
+
+git remote add origin <HTTPS link.git>
+ - Remote тоже пишется единожды когда соединяем проект с репозиторем
+Bash
+
+git branch
+
+git push -u origin master
+"""
