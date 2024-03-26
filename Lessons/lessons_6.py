74557936243d474f59786cabfe72f2bc67c10f12
e1kinbaeva <ainazikerkinbaeva2106200414@gmail.com>
2024-03-26 19:59:48 +0300

    lesson 6

---
+++ Lessons/lessons_6.py
@@ -0,0 +1,42 @@
+"""
+1) Команды для связки
+git --version
+
+git config --global user.name "YOUR USERNAME ON GITHUB"
+
+git config --global user.email "YOUR EMAIL ON GITHUB"
+"""
+
+
+
+"""
+2) Первые команды для пуша
+git init
+
+git add .
+
+git status
+
+git commit -m "First Commit"
+
+git remote add origin <HTTPS link.git>
+ - Remote тоже пишется единожды когда соединяем проект с репозиторем
+Bash
+
+git branch
+
+git push -u origin master
+"""
+
+
+"""
+3) Команды для существующего репозитории
+git add .
+git commit -m "First Commit"
+git branch
+git push 
+"""
+
+"""
+git clone
+"""
