cc8661eb65afb0b874be3fa70e123ed6c3a9ab5b
e1kinbaeva <ainazikerkinbaeva2106200414@gmail.com>
2024-03-26 21:07:09 +0300

    lesson 7 baza dannuh

--- lesson_7.py
+++ lesson_7.py
@@ -11,7 +11,19 @@ cursor  = connection.cursor()
 
 "Sozdanie tabl"
 
-cursor.execute(""" CREATE TABLE students(
-             id 
+cursor.execute(""" CREATE TABLE IF NOT EXISTS students(
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               full_name VARCHAR (30) NOT NULL,
+               attendance DOUBLE (3 , 2) DEFAULT 0.0,
+               hobby TEXT DEFAULT NULL,
+               is_payment BOOLEAN DEFAULT FALSE,
+               birthday DATE
+
+               
 )""")
+
+"int = INT, INTEGER, BIGINT"
+"str = TEXT, VARCHAR"
+"float = FLOAT, REAL, DOUBLE"
+"bool = BOOLEAN "
 
